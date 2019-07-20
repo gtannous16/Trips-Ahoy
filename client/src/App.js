b1688eb7f6dc3b4a46191e1a2453f425d15f6226
Gigi Tannous <gtannous16@gmail.com>
2019-07-20 13:04:21 -0400

    got connection to routes/,/quiz,/cruises

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import question from './components/Question';
+// import question from '../src/components/Question';
+
 
 function App() {
   return (
