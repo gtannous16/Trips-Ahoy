f75ef1b6629cbc7b84dc17a61d139a4774a66728
Margarita <m_mg489@outlook.com>
2019-07-20 00:17:07 -0400

    attempting to fix index.html not being read

--- client/src/App.js
+++ client/src/App.js
@@ -1,49 +1,26 @@
-// import React from 'react';
-// import logo from './logo.svg';
-// import './App.css';
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-// export default App;
-
-import React from 'react'
-import { Provider, Heading, Subhead } from 'rebass'
-import {
-  Hero, CallToAction, ScrollDownIndicator
-} from 'react-landing-page'
- 
-const App = props => (
-  <Provider>
-    <Hero
-      color="black"
-      bg="white"
-      backgroundImage="https://source.unsplash.com/jxaj-UrzQbc/1600x900"
-    >
-        <Heading>Name of your app</Heading>
-        <Subhead>a couple more words</Subhead>
-        <CallToAction href="/getting-started" mt={3}>Get Started</CallToAction>
-        <ScrollDownIndicator/>
-    </Hero>
-  </Provider>
-)
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
 
 export default App;
