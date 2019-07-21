66b764e99efa5014b769d8557330ced6c4b2c57a
Gigi Tannous <gtannous16@gmail.com>
2019-07-21 19:22:57 -0400

    trying to pull updates

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,38 @@
 import React from 'react';
+import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
 // import question from '../src/components/Question';
+// import Cruises from './components/Cruises'
+// import Quiz from './components/Quiz'
+import Jumbotron from './components/Jumbotron/jumbotron'
+import Header from './components/Header/header'
 
+function NoMatch() {
+  return <div>404</div>
+}
 
+function Quiz() {
+  return <div>quiz</div>
+}
+
+function Cruises() {
+  return <div>cruises</div>
+}
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Header />
+      <Jumbotron />
+      <div>
+        <Switch>
+          <Route exact path="/" component={Quiz} />
+          <Route exact path="/quiz" component={Quiz} />
+          <Route exact path="/cruises" component={Cruises} />
+          <Route component={NoMatch} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
---
+++ client/src/components/Jumbotron/jumbotron.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Jumbotron() {
+    return <div>jumbotron</div>
+}
+
+export default Jumbotron;
