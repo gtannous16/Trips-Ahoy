c3b71285ac43b5fd309e8e5b7eef55bc2f28793a
Gigi Tannous <gtannous16@gmail.com>
2019-07-21 21:43:26 -0400

    added the functionality of the quiz in app.js (still in progress)

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,14 @@
 import React from 'react';
 import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
+import quizQuestions from '../../routes/api/quizQuestions';
+import Quiz from './components/Quiz/quiz';
+import Result from './components/Quiz/Result';
 import logo from './logo.svg';
 import './App.css';
-// import question from '../src/components/Question';
-// import Cruises from './components/Cruises'
-// import Quiz from './components/Quiz'
 import Jumbotron from './components/Jumbotron/jumbotron'
 import Header from './components/Header/header'
 
-function NoMatch() {
-  return <div>404</div>
-}
 
-function Quiz() {
-  return <div>quiz</div>
-}
-
-function Cruises() {
-  return <div>cruises</div>
-}
 function App() {
   return (
     <Router>
@@ -36,4 +26,89 @@ function App() {
   );
 }
 
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+      this.stat = {
+        counter: 0,
+        questionId: 1,
+        question: '',
+        answerOptions: [],
+        answer: '',
+        answerCount: {},
+        result: ''
+      };
+
+      this.handleAnswerSelected = this.handleAnswerSelected.bind(this);
+  }
+
+  componentDidMount() {
+    const shuffledAnswerOptions = quizQuestions.map(question => 
+      this.shuffleArray(question.answers)
+      );
+      this.setState({
+        question: quizQuestions[0].question,
+        answerOptions: shuffledAnswerOptions[0]
+      });
+    }
+    
+  shuffleArray(array) {
+    var currentIndex = array.length,
+    temporaryValue,
+    randomIndex;
+
+    while (0 !== currentIndex) {
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+      
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+    return array;
+  }
+
+  handleAnswerSelected(event){
+    this.setUserAnswer(event.currentTarget.value);
+
+    if (this.state.questionId < quizQuestions.length){
+      setTimeout(() => this.setNextQuestion(), 300);
+    } else {
+      setTimeout(() => this.setResults(this.getResults()),300)
+    }
+  }
+
+  setUserAnswer(answer) {
+    this.setState((state,  props) => ({
+      answersCount: {
+        ...state.answersCount,
+        [answer]: (state.answersCount[answer] || 0) + 1
+      },
+      answer: answer
+    }));
+  }
+
+  setNextQuestion() {
+    const counter = this.state.counter + 1;
+    const questionId = this.state.questionId +1;
+
+    this.setState({
+      counter: counter,
+      questionId: questionId,
+      question: quizQuestions[counter].question,
+      answerOptions: quizQuestions[counter].answers,
+      answer: ''
+    });
+  }
+
+  getResults(){
+    const answersCount = this.state.answersCount;
+    const answersCountKeys =  Object.keys(answersCount);
+    const answersCountValues = answersCountKeys.map(key => answersCount[key]);
+    const maxAnswerCount = Math.max.apply(null, answersCountValues);
+
+    return answersCountKeys.filter(key => answersCount[key] === maxAnswerCount);
+  }
+}
 export default App;
