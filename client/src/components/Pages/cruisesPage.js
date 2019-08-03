413ec535137dd91cb6e0f794e98d20b1e23b75e1
Gigi Tannous <gtannous16@gmail.com>
2019-08-02 21:56:27 -0400

    tried to do some starter code for cruise page so when databse hooks up we can be ahead

--- client/src/components/Pages/cruisesPage.js
+++ client/src/components/Pages/cruisesPage.js
@@ -1,5 +1,31 @@
 // import React, { Component } from 'react';
-// import '../Cruises.css';
-// import cruisesPage from '../Cruises/cruises';
-// import axios from 'axios';
+// import Cruises from '../Cruises/cruises';
+// import { Col, Row, Container } from '../Grid/index';
+// import { List, ListItem } from '../List/index';
+// import { AsyncParallelBailHook } from 'tapable';
 
+// render() {
+//     return(
+//         <Container fluid>
+//             <Row>
+//                 <col size="md-6 sm-12">
+//                 <List>
+//                     {this.state.cruises.map(name =>
+//                         <ListItem key={name.availableCruises}>
+//                             <a
+//                                 href={availableCruises.link}
+//                                 target='_blank'
+//                                 rel= 'noopender noreferrer'
+//                             >
+//                                 Book This Cruise!
+//                             </a>
+//                         </ListItem>)}
+//                 </List>
+//                 </col>
+//             </Row>
+//         </Container>
+//     )
+// }
+
+
+export default cruisesPage;
