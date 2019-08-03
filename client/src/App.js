6009636af665906a35a65b47ab558b33c5a068aa
Margarita <m_mg489@outlook.com>
2019-08-03 13:21:34 -0400

    updated appJS to new path for landing page and updating landingJS to be the correct format

--- client/src/App.js
+++ client/src/App.js
@@ -2,18 +2,18 @@ import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Quiz from "./components/Pages/quizPage";
 import Cruise from "./components/Pages/cruisesPage";
-import Home from "./components/Pages/landing";
+import Home from "./components/Landing/landing";
 import Analytics from "./components/Pages/analytics";
 
-function App () {
+function App() {
   return (
     <Router>
       <div>
         <Switch>
-          <Route exact path = "/" component={Home} />
-          <Route exact path = "/quiz" component = {Quiz} />
-          <Route exact path = "/cruise" component = {Cruise} /> 
-          <Route exact path = "/analytics" component = {Analytics} /> 
+          <Route exact path="/" component={Home} />
+          <Route exact path="/quiz" component={Quiz} />
+          <Route exact path="/cruise" component={Cruise} />
+          <Route exact path="/analytics" component={Analytics} />
         </Switch>
       </div>
     </Router>
