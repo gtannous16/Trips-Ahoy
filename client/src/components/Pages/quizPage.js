c858099b6dd5595ae109a9c0776d82d8553f6915
Margarita <m_mg489@outlook.com>
2019-08-06 18:43:59 -0400

    updated results styles

--- client/src/components/Pages/quizPage.js
+++ client/src/components/Pages/quizPage.js
@@ -1,21 +1,21 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import quizQuestions from '../Quiz/quizQuestions';
-import Quiz from '../Quiz/Quiz';
-import Result from '../Quiz/Result';
-import axios from 'axios';
-import '../Quiz/Quiz.css';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import quizQuestions from "../Quiz/quizQuestions";
+import Quiz from "../Quiz/Quiz";
+import Result from "../Quiz/Result";
+import axios from "axios";
+import "../Quiz/Quiz.css";
 class quizPage extends Component {
   constructor(props) {
     super(props);
     this.state = {
       counter: 0,
       questionId: 1,
-      question: '',
+      question: "",
       answerOptions: [],
-      answer: '',
+      answer: "",
       answersCount: {},
-      result: '',
+      result: "",
       country: [],
       currentcountry: [],
       countryTodos: {}
@@ -58,7 +58,7 @@ class quizPage extends Component {
       setTimeout(() => this.setResults(this.getResults()), 300);
     }
   }
-  
+
   setUserAnswer(answer) {
     this.setState((state, props) => ({
       answersCount: {
@@ -77,7 +77,7 @@ class quizPage extends Component {
       questionId: questionId,
       question: quizQuestions[counter].question,
       answerOptions: quizQuestions[counter].answers,
-      answer: ''
+      answer: ""
     });
   }
 
@@ -86,42 +86,49 @@ class quizPage extends Component {
     const answersCountKeys = Object.keys(answersCount);
     const answersCountValues = answersCountKeys.map(key => answersCount[key]);
     const maxAnswerCount = Math.max.apply(null, answersCountValues);
-    const result = answersCountKeys.filter(key => answersCount[key] === maxAnswerCount);
-    console.log('the result from getResults is ', result )
+    const result = answersCountKeys.filter(
+      key => answersCount[key] === maxAnswerCount
+    );
+    console.log("the result from getResults is ", result);
     return result;
   }
 
-  setResults = (result) => {
+  setResults = result => {
     if (result.length === 1) {
       const personality = result[0].toLowerCase();
       const baseUrl = `/quiz/person/${personality}`;
-      
-      const url = process.env.NODE_ENV === 'production' ? baseUrl : `http://localhost:5000${baseUrl}`
 
-      axios.get(url)
-        .then((response) => {
+      const url =
+        process.env.NODE_ENV === "production"
+          ? baseUrl
+          : `http://localhost:5000${baseUrl}`;
+
+      axios
+        .get(url)
+        .then(response => {
           // handle success
           console.log(response.data);
           const countryList = Object.keys(response.data[0].availableTodos[0]);
-          console.log('result ', countryList);
+          console.log("result ", countryList);
           let todoList = {};
           for (let i = 0; i < countryList.length; i++) {
             const countryContext = countryList[i];
-            todoList[countryContext] = response.data[0].availableTodos[0][countryContext];
+            todoList[countryContext] =
+              response.data[0].availableTodos[0][countryContext];
           }
-                  
+
           this.setState({
             result: result[0],
             countryTodos: todoList,
             country: countryList
           });
         })
-        .catch(function (error) {
+        .catch(function(error) {
           // handle error
           console.log(error);
         });
     }
-  }
+  };
 
   renderQuiz() {
     return (
@@ -140,37 +147,48 @@ class quizPage extends Component {
   }
   activateTodoList(countryButton) {
     console.log(countryButton);
-    var currentcountry = this.state.countryTodos[countryButton]
-    this.setState({ currentcountry })
+    var currentcountry = this.state.countryTodos[countryButton];
+    this.setState({ currentcountry });
   }
   render() {
-    console.log('this is the state', this.state)
+    console.log("this is the state", this.state);
     return (
       <div className="App">
         <div className="App-header">
           <h2>Trips-Ahoy!</h2>
           <h2>Figure out where to go based on your personality!</h2>
         </div>
-          {this.state.result ? this.renderResult() : this.renderQuiz()}
-          {this.state.result ? (<Link to={`/quiz/cruise/${this.state.result}`} >Go To All Cruises for your Personality Type!</Link>) : ""}
-          {this.state.country.length > 0 ? this.state.country.map((countryButton, index) => (
-            <div className="countrybuttons" key={index}>
-              <button className="countryButton" onClick={() => this.activateTodoList(countryButton)}>{countryButton}</button>
-             
-            </div>
-          )) : null}
+        {this.state.result ? this.renderResult() : this.renderQuiz()}
+        {this.state.result ? (
+          <Link to={`/quiz/cruise/${this.state.result}`}>
+            Go To All Cruises for your Personality Type!
+          </Link>
+        ) : (
+          ""
+        )}
+        {this.state.country.length > 0
+          ? this.state.country.map((countryButton, index) => (
+              <div className="countrybuttons" key={index}>
+                <button
+                  className="countryButton"
+                  onClick={() => this.activateTodoList(countryButton)}
+                >
+                  {countryButton}
+                </button>
+              </div>
+            ))
+          : null}
 
-         
-
-          {this.state.currentcountry.length > 0 ? this.state.currentcountry.map(({ todo, image }, index) => (
-            <div className="container result" key={index}>
-              <h1>Something To Do:</h1>
-              <p>{todo}</p>
-              <img alt="activity to do" src={image} />
-            </div>
-          )) : null}
-        </div>
-    
+        {this.state.currentcountry.length > 0
+          ? this.state.currentcountry.map(({ todo, image }, index) => (
+              <div className="container result" key={index}>
+                <h1>Something To Do:</h1>
+                <p>{todo}</p>
+                <img alt="activity to do" src={image} />
+              </div>
+            ))
+          : null}
+      </div>
     );
   }
 }
